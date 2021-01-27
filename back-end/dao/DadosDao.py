09b03280dddd0240de96f7a98876812d417a2208
Arthur da Soler Zago <47753627+arthurdaszago@users.noreply.github.com>
2021-01-27 17:00:18 -0300

    metodo de inserção de dados de leitos

--- back-end/dao/DadosDao.py
+++ back-end/dao/DadosDao.py
@@ -135,6 +135,25 @@ class DadosDao(Dao):
                 elif index % 1000 == 0:
                     print('.', end='', flush=True)
         print('Fim', flush=True)
+	
+	def leitos_hospitais(self, params):
+		sql = """
+		    INSERT INTO leitos ( 
+			macrorregiao,
+			hospital,
+			municipio,
+			codigo_ibge_municipio,
+			regional_saude,
+			leitos_ativos,
+			leitos_ocupados,
+			leitos_disponiveis,
+			taxa_ocupacao,
+			pacientes_covid
+		    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+		"""
+
+		self.db.execute_query(sql, params)
+		self.db.conn.commit()
         ''' 
 
          ## SQL para obter casos  por município:
