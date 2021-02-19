4cfe63c923ba4b2adcd4a1af9f58fa86bc80aa94
Marcelo Cendron <marcelocendron@gmail.com>
2021-02-19 19:51:29 -0300

    Update download_databases.py

--- back-end/download_databases.py
+++ back-end/download_databases.py
@@ -5,7 +5,6 @@ import requests
 from xml.dom import minidom
 import urllib
 
-#teste...22234
 url = 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral'
 
 #headers = CaseInsensitiveDict()
